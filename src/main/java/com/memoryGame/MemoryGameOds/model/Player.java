59844e5ee7ce385d02f945459891014dcc4fd127
Allan Freitas <allangabrieldev@gmail.com>
2025-09-08 14:48:45 -0300

    feat: add model.Player

---
+++ src/main/java/com/memoryGame/MemoryGameOds/model/Player.java
@@ -0,0 +1,31 @@
+package com.memoryGame.MemoryGameOds.model;
+
+public class Player {
+    String name;
+    String nacionality;
+    Integer movements;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNacionality() {
+        return nacionality;
+    }
+
+    public void setNacionality(String nacionality) {
+        this.nacionality = nacionality;
+    }
+
+    public Integer getMovements() {
+        return movements;
+    }
+
+    public void setMovements(Integer movements) {
+        this.movements = movements;
+    }
+}

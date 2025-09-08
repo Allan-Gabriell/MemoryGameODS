f0ef3d353aa46ca6e9641e6897a1b642ee72f011
Allan Freitas <allangabrieldev@gmail.com>
2025-09-08 14:45:33 -0300

    feat: add model.Card

---
+++ src/main/java/com/memoryGame/MemoryGameOds/model/Card.java
@@ -0,0 +1,31 @@
+package com.memoryGame.MemoryGameOds.model;
+
+public class Card {
+    String name;
+    String description;
+    String imgUrl;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
+    }
+}

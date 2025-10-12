897b36a96ff0ddffda3dea06e0629162abe9c029
Allan Freitas <allangabrieldev@gmail.com>
2025-10-12 11:46:55 -0300

    refactor: function loadCards

--- src/main/java/com/memoryGame/MemoryGameOds/controller/GameControlle.java
+++ src/main/java/com/memoryGame/MemoryGameOds/controller/GameControlle.java
@@ -1,13 +1,13 @@
 package com.memoryGame.MemoryGameOds.controller;
 
+import com.memoryGame.MemoryGameOds.DTO.CardResponseDTO;
 import com.memoryGame.MemoryGameOds.DTO.PlayerResquestDTO;
 import com.memoryGame.MemoryGameOds.model.Game;
 import com.memoryGame.MemoryGameOds.model.Player;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("game")
@@ -21,4 +21,8 @@ public class GameControlle {
         game.addPlayer(player);
     }
 
+    @GetMapping("/load-cards")
+    public List<CardResponseDTO> loadCards(){
+        return game.loadCards();
+    }
 }
--- src/main/java/com/memoryGame/MemoryGameOds/model/Game.java
+++ src/main/java/com/memoryGame/MemoryGameOds/model/Game.java
@@ -1,5 +1,7 @@
 package com.memoryGame.MemoryGameOds.model;
 
+import com.memoryGame.MemoryGameOds.DTO.CardResponseDTO;
+import com.memoryGame.MemoryGameOds.repository.CardRepository;
 import com.memoryGame.MemoryGameOds.repository.PlayerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -13,6 +15,9 @@ public class Game {
     @Autowired
     private PlayerRepository playerRepository;
 
+    @Autowired
+    private CardRepository cardRepository;
+
     private List<Player> players = new ArrayList<>();
     private List<Card> cards = new ArrayList<>();
     private boolean isGameActive = false;
@@ -21,9 +26,14 @@ public class Game {
         playerRepository.save(player);
     }
 
-    public void loadCards(List<Card> cardList){
-        cards.clear();
-        cards.addAll(cardList);
-        Collections.shuffle(cards);
+    public List<CardResponseDTO> loadCards(){
+        List<CardResponseDTO> pairedList = cardRepository.findAll().stream()
+                .flatMap(card -> List.of(new CardResponseDTO(card), new CardResponseDTO(card)).stream())
+                .toList();
+
+        List<CardResponseDTO> shuffledList = new ArrayList<>(pairedList);
+        Collections.shuffle(shuffledList);
+
+        return shuffledList;
     }
 }
